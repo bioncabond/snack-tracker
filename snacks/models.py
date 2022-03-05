7f51f867b8c5064b5540778d619b6ae3d43413dd
Bionca Bond <bioncab@zillowgroup.com>
2022-03-04 17:32:47 -0800

    all working except for description for some reason?

--- snacks/models.py
+++ snacks/models.py
@@ -1,9 +1,13 @@
-from tkinter import CASCADE
+from unicodedata import name 
 from django.db import models
 from django.contrib.auth import get_user_model 
 
-# Create your models here.
-class snack(models.Model): 
+#Create your models here.
+class Snack(models.Model): 
     name = models.CharField(max_length=64)
     purchaser = models.ForeignKey(get_user_model(),on_delete = models.CASCADE)
-    description = models.TextField
+    # description = models.TextField()
+
+    def __str__(self):
+        """String for representing the MyModuleName object (in Admin site etc.)."""
+        return self.name
